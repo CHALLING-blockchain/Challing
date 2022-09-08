eae2d364cbf5eb2e99c3e56cadca0670987158e6
josajang98 <na0173@naver.com>
2022-09-08 23:54:09 +0900

    feat: 🎸 react-redux skeleton

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+
+import { BrowserRouter } from '/react-router-dom'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
