797d82ed7518197b7818c6f9f19d6d6d2afa07cf
na0173 <na0173@naver.com>
2022-09-08 15:38:19 +0900

    feat: 🎸 스켈레톤작성

--- backend/src/main/java/com/ssafy/db/entity/User.java
+++ backend/src/main/java/com/ssafy/db/entity/User.java
@@ -4,10 +4,7 @@ package com.ssafy.db.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -21,5 +18,7 @@ public class User {
     private String email;
     private String nickname;
     private String picURL;
+
+
 
 }
