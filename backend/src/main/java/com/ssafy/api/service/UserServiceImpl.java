c283e67d420d49f94fc346df8d56d80041aa42ac
송제영 <wp29dud@naver.com>
2022-09-12 13:21:51 +0900

    feat: 🎸 add user service impl

---
+++ backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.ssafy.api.service;
+
+import com.ssafy.db.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public User getUserByEmail(String email) {
+        return null;
+    }
+}
--- backend/src/test/java/com/ssafy/BackendApplicationTests.java
+++ backend/src/test/java/com/ssafy/BackendApplicationTests.java
@@ -1,13 +1,28 @@
 package com.ssafy;
 
+import com.ssafy.db.entity.User;
+import com.ssafy.db.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+	@Autowired
+	UserRepository userRepository;
+
 	@Test
 	void contextLoads() {
+		User user = new User();
+		user.setEmail("email000");
+		user.setNickname("nickname000");
+		user.setPicURL("url000");
+		userRepository.save(user);
+		User userFound = userRepository.findByEmail("email000").get();
+		assertEquals(user.getId(), userFound.getId(), "user id not equals");
 	}
 
 }
