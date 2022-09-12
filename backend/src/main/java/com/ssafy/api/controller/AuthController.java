1650baa33accd66cbdbab1329756bf08cf895c65
송제영 <wp29dud@naver.com>
2022-09-12 15:48:41 +0900

    refactor: 💡 update auth controller

--- backend/src/main/java/com/ssafy/api/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/api/controller/AuthController.java
@@ -10,12 +10,8 @@ import com.ssafy.db.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,22 +24,16 @@ public class AuthController {
     private final KakaoApi kakaoApi;
     private final UserService userService;
 
-    @GetMapping("/login")
-    public ResponseEntity<?> login(@RequestParam("code") String code) {
-        Map<String, Object> resultMap = new HashMap<>();
-        HttpStatus status = null;
-        logger.debug("code " + code);
-        // 1번 인증코드 요청 전달
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> requestBody) {
+        String code = requestBody.get("code");
+        logger.debug("code: {}", code);
 
         String accessToken = kakaoApi.getAccessToken(code);
+        logger.debug("accessToken: {}", accessToken);
 
-        logger.debug("accessToken : " + accessToken);
-        // 2번 인증코드로 토큰 전달
         HashMap<String, Object> userInfo = kakaoApi.getUserInfo(accessToken);
-
-        logger.debug("\n\nlogin info: {}\n", userInfo.toString());
-
-        String token = JwtTokenUtil.getToken((String) userInfo.get("email"));
+        logger.debug("login info: {}", userInfo.toString());
 
         String email = (String) userInfo.get("email");
 
@@ -65,11 +55,12 @@ public class AuthController {
                     .build());
         }
 
+        String token = JwtTokenUtil.getToken(email);
+
         return BaseResponse.success(UserLoginResponse.builder()
                 .visited(true)
                 .jwt(token)
                 .user(UserResponse.of(user))
                 .build());
-
     }
 }
