482a918c8b0ead4828530c7138bba6081534e14a
lye2i <65855978+lye2i@users.noreply.github.com>
2022-09-12 13:26:45 +0900

    feat: 🎸 add authcontroller

---
+++ backend/src/main/java/com/ssafy/api/controller/AuthController.java
@@ -0,0 +1,18 @@
+package com.ssafy.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @GetMapping("/login")
+    public ResponseEntity<?> login(){
+        return ResponseEntity.ok("okay");
+    }
+}
