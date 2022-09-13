f1ef095280ad837ddae1d35c241a5e6ec47644ac
차송희 <chasonghui@naver.com>
2022-09-13 22:31:06 +0900

    feat: 🎸 metamask지갑연동

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+
+import {useWeb3React} from '@web3-react/core';
+import {injected} from './lib/connectors';
+
 import './App.css';
 
 function App() {
+  const {
+    chainId,
+    account,
+    active,
+    activate,
+    deactivate
+  } = useWeb3React();
+
+  const handleConnect = () => {
+    if(active) {
+      deactivate();
+      return;
+    }
+
+  activate(injected,(error)=>{
+      if('/No Ethereum provider was found on window.ethereum/'.test(error)){
+        window.open('https://metamask.io/download.html');
+      }
+  });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+        <div>
+          <p>Account: {account}</p>
+          <p>ChainId: {chainId}</p>
+        </div>
+        <div>
+          <button type="button" onClick={handleConnect}>{active?'disconnect':'connect'}</button>
+        </div>
     </div>
-  );
+ )
 }
 
 export default App;
