2c896019c211b91b1b3a498bcdcf3fe555b53f2c
na0173 <na0173@naver.com>
2022-09-14 01:43:48 +0900

    feat: 🎸 add getMyChallenge

--- truffle/scripts/app.js
+++ truffle/scripts/app.js
@@ -8,14 +8,14 @@ const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 
 // netListening();
 
-const artifact = require("../build/contracts/ChallengeContract.json");
+const artifact = require("../../frontend/src/contracts/ChallengeContract.json");
 
 const test = async () => {
   const networkId = await web3.eth.net.getId();
   const { abi } = artifact;
   const address = artifact.networks[networkId].address;
   const contract = new web3.eth.Contract(abi, address);
-
+  
   const daliyChallenge = {
     challengeId:0,
     interestId: 0,
@@ -65,59 +65,66 @@ const test = async () => {
     userDeposit:[],
   }
   
-  // const r1 = await contract.methods
-  //   .createDaliyChallenge(daliyChallenge)
-  //   .send({
-  //     from: "0x278b6c8a8b938d7c6d1cdb055d95783379b0b842",
-  //     gasLimit: 3_000_000,
-  //   })
-  //   .catch(console.error);
-  // console.log(r1)
-
-  // const r2 = await contract.methods
-  //   .createDonationChallenge(donationChallenge)
-  //   .send({
-  //     from: "0x278b6c8a8b938d7c6d1cdb055d95783379b0b842",
-  //     gasLimit: 3_000_000,
-  //   })
-  //   .catch(console.error);
-  // console.log(r2)
-  
-  
-
-  // const r4 = await contract.methods
-  // .joinChallenge(0,10)
-  // .send({
-  //   from: "0x278b6c8a8b938d7c6d1cdb055d95783379b0b842",
-  //   gasLimit: 3_000_000,
-  //   value: 1e18
-  // })
-  // .catch(console.error);
-
-  // const r5 = await contract.methods
-  // .getUserChallenge()
-  // .call({
-  //   from: "0x278b6c8a8b938d7c6d1cdb055d95783379b0b842",
-  // })
-  // .catch(console.error);
-  // console.log(r5)
-  const r3 = await contract.methods
-    .getAllChallenge()
-    .call({
-      from: "0x278b6c8a8b938d7c6d1cdb055d95783379b0b842",
+  const r1 = await contract.methods
+    .createDaliyChallenge(daliyChallenge)
+    .send({
+      from: "0xeaa1df164d12a35423b243fe8197cd47b8351353",
+      gasLimit: 3_000_000,
     })
     .catch(console.error);
-  console.log(r3)
-  const challenges={}
-  r3[0].forEach((id,index) => {
-    const challenge=Object.assign({},r3[2][index])
-    const size=Object.keys(challenge).length
-    for(let i=0;i<size/2;i++){
-      delete challenge[i]
-    }
-    challenges[Number(id)]=challenge
-  });
-  console.log(challenges)
+  console.log(r1)
+
+  const r2 = await contract.methods
+    .createDonationChallenge(donationChallenge)
+    .send({
+      from: "0xeaa1df164d12a35423b243fe8197cd47b8351353",
+      gasLimit: 3_000_000,
+    })
+    .catch(console.error);
+  console.log(r2)
+  
+  const r4 = await contract.methods
+  .joinChallenge(0,0)
+  .send({
+    from: "0xeaa1df164d12a35423b243fe8197cd47b8351353",
+    gasLimit: 3_000_000,
+    value: 1e18
+  })
+  .catch(console.error);
+
+  const r5 = await contract.methods
+  .getMyChallenge(1)
+  .call({
+    from: "0x278b6c8a8b938d7c6d1cdb055d95783379b0b842",
+  })
+  .catch(console.error);
+  console.log(r5)
+  // const r3 = await contract.methods
+  //   .getAllChallenge()
+  //   .call({
+  //     from: "0xeaa1df164d12a35423b243fe8197cd47b8351353",
+  //   })
+  //   .catch(console.error);
+
+  // const challenges={}
+  // r3[0].forEach((id,index) => {
+  //   const challenge=Object.assign({},r3[2][index])
+  //   const size=Object.keys(challenge).length
+  //   for(let i=0;i<size/2;i++){
+  //     delete challenge[i]
+  //   }
+  //   challenges[Number(id)]=challenge
+  // });
+
+  // r3[1].forEach((id,index) => {
+  //   const challenge=Object.assign({},r3[3][index])
+  //   const size=Object.keys(challenge).length
+  //   for(let i=0;i<size/2;i++){
+  //     delete challenge[i]
+  //   }
+  //   challenges[Number(id)]=challenge
+  // });
+  // console.log(challenges)
 
 };
 
