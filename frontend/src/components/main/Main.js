f3a0890216e02dfb809eeb0b81962e19f7531eba
ssomm-k <kww0915@gmail.com>
2022-09-18 17:34:59 +0900

    feat: 🎸 Menu.js 생성

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Main.css';
+import Menu from '../Menu'
 
 function Main() {
     return (
-        <div>
+        <div className='Main'>
             <h2>
                 Main
             </h2>
+            <Menu/>
         </div>
     );
 };
