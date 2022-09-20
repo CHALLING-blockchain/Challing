ef5383fc77a7773b95c06b3273a9f4be07a799ea
차송희 <chasonghui@naver.com>
2022-09-20 15:01:04 +0900

    test: 💍 sendTx 테스트 코드 주석

--- frontend/src/hooks/useWeb3.js
+++ frontend/src/hooks/useWeb3.js
@@ -39,58 +39,48 @@ function useWeb3(setIsLoading, setErrorMessage, exist, setExist) {
       setExist(true);
 
       // send transaction  ---------------------------------
-      var nonce = 0;
-      await web3.eth.getTransactionCount(accounts[0]).then(function (result) {
-        nonce = result;
-      });
-      //console.log("nonce:", nonce);
+      // var nonce = 0;
+      // await web3.eth.getTransactionCount(accounts[0]).then(function (result) {
+      //   nonce = result;
+      // });
+      // //console.log("nonce:", nonce);
 
-      var gasPrice = 0;
-      await web3.eth.getGasPrice().then(function (result) {
-        gasPrice = result;
-      });
-      //console.log("gasPrice:", gasPrice);
+      // var gasPrice = 0;
+      // await web3.eth.getGasPrice().then(function (result) {
+      //   gasPrice = result;
+      // });
+      // //console.log("gasPrice:", gasPrice);
 
-      var value = await web3.utils.toWei("0.1", "ether");
-      //console.log("value:", value);
+      // var value = await web3.utils.toWei("0.1", "ether");
+      // //console.log("value:", value);
 
-      var gasLimit = 0;
-      await web3.eth
-        .estimateGas({
-          to: "0x301E1528bAD61177eF8Ff89bD4ad6760581e5409",
-          from: accounts[0],
-          value: value,
-        })
-        .then(function (result) {
-          gasLimit = result;
-        });
+      // var gasLimit = 0;
+      // await web3.eth
+      //   .estimateGas({
+      //     to: "0x301E1528bAD61177eF8Ff89bD4ad6760581e5409",
+      //     from: accounts[0],
+      //     value: value,
+      //   })
+      //   .then(function (result) {
+      //     gasLimit = result;
+      //   });
 
-      var txObject = {
-        nonce: nonce,
-        gasPrice: gasPrice,
-        gasLimit: 60000,
-        to: "0x301E1528bAD61177eF8Ff89bD4ad6760581e5409",
-        from: accounts[0],
-        value: value,
-        data:"6368616c6c656e6765"
-      };
-      console.log("txObject: ", txObject);
+      // var txObject = {
+      //   nonce: nonce,
+      //   gasPrice: gasPrice,
+      //   gasLimit: 60000,
+      //   to: "0x301E1528bAD61177eF8Ff89bD4ad6760581e5409",
+      //   from: accounts[0],
+      //   value: value,
+      //   data:"6368616c6c656e6765"
+      // };
+      // console.log("txObject: ", txObject);
 
-      var hashObject = 0;
+      // var hashObject = 0;
       // await web3.eth.sendTransaction(txObject).then(function (receipt) {
       //   hashObject = receipt;
       // });
       // send transaction end -------------------------------
-
-      // transaction details ------------------------------
-      //console.log("hashObject", hashObject);
-      //console.log("my txHash: ", hashObject.transactionHash);
-      var hashToString = hashObject.transactionHash.toString();
-      //console.log(hashToString);
-      web3.eth.getTransaction(hashToString, function (error, result) {
-        console.log("transaction detail: ", result);
-      });
-      // transaction details end --------------------------
 
     } catch (error) {
       setErrorMessage(error.message);
