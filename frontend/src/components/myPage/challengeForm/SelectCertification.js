2aa48e557f3de44cb0d239b956b6989e4ff16921
ssomm-k <kww0915@gmail.com>
2022-09-21 17:38:03 +0900

    feat: 🎸 Create Challenge Form 70%

---
+++ frontend/src/components/myPage/challengeForm/SelectCertification.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import './challengeForm.css';
+function SelectCertification({formCnt,setFormCnt,nTimesAWeek,setNTimesAWeek,authentications,setAuthentications}){
+  const[list,setList] = useState([]);
+  function NextButton(){
+    return(
+      <button className="NextButton" onClick={()=>{setFormCnt(formCnt+1)}}>Next( {formCnt} / 8)</button>
+    )
+  }
+  return (
+    <div>
+      <div className="BackMyPage">
+        <svg onClick={()=>{setFormCnt(formCnt-1)}} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M11.08 1.99341C10.7534 1.66675 10.2267 1.66675 9.90004 1.99341L4.36004 7.53341C4.10004 7.79341 4.10004 8.21341 4.36004 8.47341L9.90004 14.0134C10.2267 14.3401 10.7534 14.3401 11.08 14.0134C11.4067 13.6867 11.4067 13.1601 11.08 12.8334L6.25337 8.00008L11.0867 3.16675C11.4067 2.84675 11.4067 2.31341 11.08 1.99341Z" fill="#444444"/>
+        </svg>
+        <p>챌린지 개설하기</p>
+      </div>
+      <div>
+        <p className="FormHeader">챌린지 인증의 디테일을 정해주세요.</p>
+        <p className="FormEx">챌린지의 인증 빈도와 인증 가능시간을 정해<br/>
+                              챌린지의 성공확률을 높여보세요.<br/>
+                              챌린지 개설 후 디테일 설정을 변경할 수 없습니다.<br/> </p>
+      </div>
+      <div>
+        <p>인증 빈도</p>
+        <p>주간 참여 횟수와 하루 인증 횟수를 작성해주세요. 숫자만 입력 가능합니다.</p>
+        <label for="week">주</label>
+        <input id="week" type="number" min="1" max="7"/>회
+        <label for="week">하루</label>
+        <input id="week" type="number" min="1" max="3"/>회
+      </div>
+      <div>
+        <p>인증 가능시간</p>
+      </div>
+      {list.length === 4   ? <NextButton/> : <div className="NoNextButton">Next( {formCnt} / 8)</div>}
+    </div>
+  );
+}
+export default SelectCertification;
