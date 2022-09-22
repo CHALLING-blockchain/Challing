87d5c3d52bb3f9d7275026648ae2d87ac8cc3635
차송희 <chasonghui@naver.com>
2022-09-22 15:32:52 +0900

    feat: 🎸 redux-persist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import store from "./app/store";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
 const container = document.getElementById("root");
 const root = createRoot(container);
+
+let persistor = persistStore(store);
 
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
