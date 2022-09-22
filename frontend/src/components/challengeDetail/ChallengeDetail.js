d03321643e5ddbae21f07ddc95cc9d1bf5dac581
kiki <unhhyyee66@gmail.com>
2022-09-22 15:09:26 +0900

    style: 💄 challenge detail page

--- frontend/src/components/challengeDetail/ChallengeDetail.js
+++ frontend/src/components/challengeDetail/ChallengeDetail.js
@@ -3,13 +3,95 @@ import { useNavigate } from "react-router-dom";
 import styles from "./ChallengeDetail.module.css"
 import back from "../../img/test-back.jpg"
 import profile from "../../img/profile-basic.png"
+import person from "../../img/person.png"
+import dollar from "../../img/dollarCoin.png"
+import eth from "../../img/ethCoin.png"
+import calender from "../../img/calender.png"
+
+function TopBox(){
+    return (
+      <div>
+        {/* 유저닉네임, 타이틀, 해시태그 */}
+        <div className={styles.paddingBox}>
+          <div className={styles.imgText}>
+            <img src={profile} alt="" />
+            <span>커다란 솜사탕</span>
+          </div>
+          <span style={{ fontSize: "16px", fontWeight: "bold", margin:'4px' }}>
+            영어, 외국어 10문장 쓰기
+          </span>
+          <div className={styles.Tags}>
+            <span className={styles.Tag}>4주동안</span>
+            <span className={styles.Tag}>매일매일</span>
+          </div>
+          {/* 참가인원수, 예치금 */}
+          <div className={styles.subtext}>
+            <div className={styles.imgText}>
+              <img src={person} alt="personChar" />
+              <span>현재 3명</span>
+            </div>
+            <div className={styles.imgText}>
+              <img src={dollar} alt="" />
+              <span>0.05 eth</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+function PeriodBox(){
+    return (
+        <div className={styles.paddingBox}>
+            <div className={styles.imgText}>
+                <img src={calender} alt="" />
+                <span style={{fontSize:'16px'}}>챌린지 기간</span>
+            </div>
+            <div>
+                <p style={{margin:'4px'}}>9월 5일(월) ~ 9월 18일(일)</p>
+            </div>
+        </div>
+    )
+}
+
+function RefundPolicy(){
+    return (
+      <div className={styles.paddingBox}>
+        <div className={styles.imgText}>
+          <img src={eth} alt="" />
+          <span style={{ fontSize: "16px" }}>환급정책</span>
+        </div>
+        <div className={styles.refundBox}>
+          <div className={styles.oneLine}>
+            <span className={styles.percentage}>100% 성공</span>
+            <span className={styles.policy}>참가비 전액 환급 + 성공 리워드</span>
+          </div>
+          <div className={styles.oneLine}>
+            <span className={styles.percentage}>99%이하 80%이상</span>
+            <span className={styles.policy}>참가비 전액 환급</span>
+          </div>
+          <div className={styles.oneLine}>
+            <span className={styles.percentage}>79%이하 40%이상</span>
+            <span className={styles.policy}>참가비 일부 환급</span>
+          </div>
+          <div className={styles.oneLine}>
+            <span className={styles.percentage}>40% 미만</span>
+            <span className={styles.policy}>환급금 없음</span>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+
 
 function ChallengeDetail() {
     const navigate= useNavigate();
     return (
       <div>
+        {/* 뒤로가기, 공유, 백드롭 */}
         <div className={styles.header}>
-          <div className={styles.icons} onClick={()=> navigate(-1)}>
+          <div className={styles.icons} onClick={() => navigate(-1)}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -45,19 +127,13 @@ function ChallengeDetail() {
           </div>
         </div>
         <img className={styles.backImg} src={back} alt="challegePhoto" />
-        <div className={styles.titles}>
-            <div className={styles.user}>
-                <img style={{margin:'4px', width:'16px', height:'16px', borderRadius:'20px'}} src={profile} alt="" />
-                <span style={{fontSize:'12px'}}>커다란 솜사탕</span>
-            </div>
-            <div className={styles.title}>
-                <span style={{fontSize:'16px', fontWeight:'bold'}}>영어, 외국어 10문장 쓰기</span>
-            </div>
-            <div className={styles.Tags}>
-                <span className={styles.Tag}>4주동안</span>
-                <span className={styles.Tag}>매일매일</span>
-            </div>
-        </div>
+
+        <TopBox></TopBox>
+        <hr className={styles.hrTag} />
+        <PeriodBox></PeriodBox>
+        <hr className={styles.hrTag} />
+        <RefundPolicy></RefundPolicy>
+
       </div>
     );
 }
