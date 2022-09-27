d8d5b169f9123dd83e506525df8d620c987c49b9
ssomm-k <kww0915@gmail.com>
2022-09-27 22:22:14 +0900

    fix: 🐛 warning kill

--- frontend/src/components/myPage/challengeForm/CreateFinal.js
+++ frontend/src/components/myPage/challengeForm/CreateFinal.js
@@ -88,11 +88,11 @@ function CreateFinal({selects,formCnt,setFormCnt}){
             width="16"
             height="16"
             fill="currentColor"
-            class="bi bi-chevron-left"
+            className="bi bi-chevron-left"
             viewBox="0 0 16 16"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
             />
           </svg>
