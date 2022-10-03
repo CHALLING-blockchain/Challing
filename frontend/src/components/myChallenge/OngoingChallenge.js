96ef77da380b0f54362c903c74c224748a625884
ssomm-k <kww0915@gmail.com>
2022-10-03 16:17:27 +0900

    fix: 🐛 마이페이지 참가한 챌린지 스크롤

--- frontend/src/components/myChallenge/OngoingChallenge.js
+++ frontend/src/components/myChallenge/OngoingChallenge.js
@@ -24,15 +24,15 @@ function Header() {
           width="16"
           height="16"
           fill="currentColor"
-          class="bi bi-chevron-left"
+          className="bi bi-chevron-left"
           viewBox="0 0 16 16"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
           />
         </svg>
-        <p style={{ fontSize: "20px", margin: "auto" }}>진행중 챌린지</p>
+        <p style={{ fontSize: "20px", margin: "auto" }}>참가한 챌린지</p>
         <div></div>
       </div>
     </div>
